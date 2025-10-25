c9907413df97b067935a3e58f8d9219f25ca29ce
pastaya <contact@pastaya.net>
2025-10-25 19:16:20 +0200

    OOPS took down my website

--- serverServices.nix
+++ serverServices.nix
@@ -35,13 +35,16 @@
           proxyPass = "http://localhost:8082";
         };
         default = false;
-        extraConfig = ''
-          ssl_verify_depth 2;
-          log_format main '$remote_addr - $remote_user [$time_local] "$request" '
-                          '$status $body_bytes_sent "$http_referer" '
-                          '"$http_user_agent" "$http_x_forwarded_for"';
-          access_log /var/log/nginx/access.log main;
-        '';
+
+        # haslity comment this before fixing it
+        
+        # extraConfig = ''
+        #   ssl_verify_depth 2;
+        #   log_format main '$remote_addr - $remote_user [$time_local] "$request" '
+        #                   '$status $body_bytes_sent "$http_referer" '
+        #                   '"$http_user_agent" "$http_x_forwarded_for"';
+        #   access_log /var/log/nginx/access.log main;
+        # '';
       };
     };
   };
