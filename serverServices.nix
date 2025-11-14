c334663158d9f3174f356fa6daec15e64c3215b1
pastaya <contact@pastaya.net>
2025-11-14 23:35:26 +0100

    lmao why is it using ipv6

--- serverServices.nix
+++ serverServices.nix
@@ -104,7 +104,7 @@
       openFirewall = true;
       eula = true;
       # good luck
-      jvmOpts = "-Xms2G -Xmx3G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:MaxGCPauseMillis=75 -XX:G1HeapRegionSize=8M -XX:InitiatingHeapOccupancyPercent=20 -XX:G1NewSizePercent=20 -XX:G1ReservePercent=15 -XX:SurvivorRatio=16";
+      jvmOpts = "-Xms2G -Xmx3G -XX:+UseG1GC -Djava.net.preferIPv4Stack=true -XX:+UnlockExperimentalVMOptions -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:MaxGCPauseMillis=75 -XX:G1HeapRegionSize=8M -XX:InitiatingHeapOccupancyPercent=20 -XX:G1NewSizePercent=20 -XX:G1ReservePercent=15 -XX:SurvivorRatio=16";
       package = pkgs.papermc;
       serverProperties = {
         motd = "welcome from NixOS!";
