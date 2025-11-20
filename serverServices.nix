1954e1947b32be40cb85bf8675e0efbd1a57bc12
pastaya <contact@pastaya.net>
2025-11-20 19:23:41 +0100

    use newer jre

--- serverServices.nix
+++ serverServices.nix
@@ -117,7 +117,7 @@ in {
           enable = true;
           openFirewall = true;
           jvmOpts = "-Xms512M -Xmx3G -XX:+UseG1GC -Djava.net.preferIPv4Stack=true -XX:+UnlockExperimentalVMOptions -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:MaxGCPauseMillis=75 -XX:G1HeapRegionSize=8M -XX:InitiatingHeapOccupancyPercent=20 -XX:G1NewSizePercent=20 -XX:G1ReservePercent=15 -XX:SurvivorRatio=16";
-          package = pkgs.fabricServers.${serverVersion}.override {loaderVersion = fabricVersion;};
+          package = pkgs.fabricServers.${serverVersion}.override {loaderVersion = fabricVersion; jre_headless = pkgs.graalvmPackages.graalvm-oracle;};
           serverProperties = {
             motd = "\"lifestyle\" server lmao";
             allow-nether = false;
