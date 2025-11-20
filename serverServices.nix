4a06141bf2db0b21090cb30d8342f3fc168a4b6f
pastaya <contact@pastaya.net>
2025-11-20 22:08:06 +0100

    change motd

--- serverServices.nix
+++ serverServices.nix
@@ -145,7 +145,7 @@ in {
       jvmOpts = "-Xms512M -Xmx3G -XX:+UseG1GC -Djava.net.preferIPv4Stack=true -XX:+UnlockExperimentalVMOptions -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:MaxGCPauseMillis=75 -XX:G1HeapRegionSize=8M -XX:InitiatingHeapOccupancyPercent=20 -XX:G1NewSizePercent=20 -XX:G1ReservePercent=15 -XX:SurvivorRatio=16";
       package = pkgs.fabricServers.${serverVersion}.override {loaderVersion = fabricVersion;};
       serverProperties = {
-        motd = "\"lifestyle\" server lmao";
+        motd = "'lifestyle' server";
         allow-nether = false;
       };
     };
