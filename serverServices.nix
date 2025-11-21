bddde2a4b9d4d23d9735c163609206c5530a1fd9
pastaya <contact@pastaya.net>
2025-11-21 23:50:42 +0100

    well that didn't work

--- serverServices.nix
+++ serverServices.nix
@@ -109,7 +109,7 @@ in {
     # };
 
     minecraft-servers = {
-      enable = true;
+      enable = false;
       eula = true;
       dataDir = "/mc";
 
@@ -141,7 +141,7 @@ in {
     };
 
     minecraft-server = {
-      enable = false;
+      enable = true;
       eula = true;
       openFirewall = true;
       jvmOpts = "-Xms512M -Xmx3G -XX:+UseG1GC -Djava.net.preferIPv4Stack=true -XX:+UnlockExperimentalVMOptions -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:MaxGCPauseMillis=75 -XX:G1HeapRegionSize=8M -XX:InitiatingHeapOccupancyPercent=20 -XX:G1NewSizePercent=20 -XX:G1ReservePercent=15 -XX:SurvivorRatio=16";
